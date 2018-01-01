4d4382fb86339271ebb6bbaf0baa365005e1aab4
ravindersingh17 <n30.2006@gmail.com>
2018-01-01 14:40:19 +0530

    m

--- hangouts/plugins/tpb.py
+++ hangouts/plugins/tpb.py
@@ -10,6 +10,7 @@ logger = logging.getLogger(__name__)
 TPB_HOST = "w00t.in"
 TPB_PORT = 8085
 SSH_PORT = 8082
+SSH_USER = "ravinder"
 
 def _initialise(bot):
 
@@ -56,7 +57,7 @@ async def process_message(bot, event, command):
 
             if not tpbonline:
                 await bot.coro_send_message(event.conv.id_, "Here is where we will start the server")
-                await execcommand("ssh {} -p {} vbox start \"Ubuntu Server VM\"".fomat(TPB_HOST, SSH_PORT))
+                await execcommand('ssh {}@{} -p {} "vbox start \\"Ubuntu Server VM\\"'.format(SSH_USER, TPB_HOST, SSH_PORT))
                 await bot.coro_send_message(event.conv.id_, "Started the tpb server")
             else:
                 await bot.coro_send_message(event.conv.id_, "tpb is online")
