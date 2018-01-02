71dda02f9884ca8d93cf436cac758fa71a162e5b
ravindersingh17 <n30.2006@gmail.com>
2018-01-02 12:23:40 +0530

    Changed to async

--- modtpb/piratebay.py
+++ modtpb/piratebay.py
@@ -1,15 +1,18 @@
 import re
 from bs4 import BeautifulSoup as BS
-import requests
+import aiohttp
 
-def getsearches(search):
+async def getsearches(search):
     SITE = 'https://tpbunblocked.org'
     results = []
 
     rep = lambda x, y: x + y if len(y) == 2 else x + "0" + y
     match = re.search("(\d{1,2})-(\d{1,2})", search)
     if match: search = re.sub("(\d{1,2})-(\d{1,2})", rep("S", match.group(1)) + rep("E", match.group(2)), search)
-    response = requests.get(SITE+'/s/', params={'q' : search}).text
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(SITE + '/s/', params = {"q": search}) as serverres:
+            response = await serverres.text()
 
     soup = BS(response, 'html.parser')
     links = soup.find_all('a','detLink')
@@ -21,10 +24,11 @@ def getsearches(search):
 
     for s in seedleech:
         seedleechlist.append(s.text)
+    returnData = []
     for i in range(len(links)):
         detail = re.search("Uploaded\s*(.*?), Size\s*(.*?), ULed by (.*)", desc[i].text)
         print(detail.group(1), detail.group(2), detail.group(3))
-        yield {
+        returnData.append({
                 'text': links[i].text,
                 'date': detail.group(1),
                 'size': detail.group(2),
@@ -32,9 +36,12 @@ def getsearches(search):
                 'desc': desc[i].text,
                 'seeders': (seedleechlist[i*2], seedleechlist[i*2+1]),
                 'link': SITE + links[i]['href']
-                }
+                })
+    return returnData
 
-def gettorrent(link):
-    response = requests.get(link).text
+async def gettorrent(link):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(link) as serverres:
+            response = await serverres.text()
     soup = BS(response, 'html.parser')
     return soup.find('a', title='Get this torrent')['href']
